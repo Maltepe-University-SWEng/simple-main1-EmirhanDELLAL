f325fec5722c2b75f470860940e5fa7f9b0d6550
emiro <230704001@st.maltepe.edu.tr>
2025-02-25 14:39:02 +0300

    Mission accomplished

--- module1/src/tr/edu/maltepe/oop/Simple_main.java
+++ module1/src/tr/edu/maltepe/oop/Simple_main.java
@@ -3,7 +3,7 @@ public  class Simple_main {
 
  public static void main(String []args){
         //
-       System.out.println("Emirhan DELLAL");
+       System.out.println("Emirhan  DELLAL");
         }
 }
 
